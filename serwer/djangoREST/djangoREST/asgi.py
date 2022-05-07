1e53c1ab384cb8166d2055ce6acd8c6c259c8faa
Piotr Walczak <piotrwalczak98@gmail.com>
2022-05-07 17:51:25 +0200

    refactor

--- serwer/djangoREST/djangoREST/asgi.py
+++ serwer/djangoREST/djangoREST/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for serwer project.
+ASGI config for djangoREST project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'serwer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoREST.settings')
 
 application = get_asgi_application()
--- serwer/djangoREST/djangoREST/wsgi.py
+++ serwer/djangoREST/djangoREST/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for serwer project.
+WSGI config for djangoREST project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'serwer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoREST.settings')
 
 application = get_wsgi_application()

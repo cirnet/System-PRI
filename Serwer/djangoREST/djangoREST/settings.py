f155c913f6cb485649d12ff53b5423be9f7ba9f0
Piotr Walczak <piotrwalczak98@gmail.com>
2022-05-08 20:20:34 +0200

    a

--- Serwer/djangoREST/djangoREST/settings.py
+++ Serwer/djangoREST/djangoREST/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&yr^5!2=+0=kjr7vub2pr1a3!7eg6pmn^*yi$q0l+sskt^gm%!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'djangoREST.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres_pri',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

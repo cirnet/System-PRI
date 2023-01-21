1d02f9079868ad6345a310f67a39a52470978ca1
cirnet <78275621+cirnet@users.noreply.github.com>
2023-01-21 23:53:36 +0100

    poprawki profil z tokenem

--- frontend/src/auth/Profile.js
+++ frontend/src/auth/Profile.js
@@ -1,46 +1,33 @@
 import React, { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import LoginContext from "../context/LoginContext";
-import { useCookies } from "react-cookie";
-import Cookies from "js-cookie";
+
 
 // import Cookies from 'js-cookie';
 export default function Profile() {
-  const [cookies, setCookie] = useCookies();
-
-  // const user = JSON.parse(localStorage.getItem('user'));
-
-  // const handleLogout = () => {
-  //   localStorage.removeItem("accessToken");
-  //   localStorage.removeItem("user");
-  //   window.location.href = "/";
-  // };
-
+ const [user,setUser] = useState("")
   useEffect(() => {
     const requestOptions = {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: "Bearer " + String(localStorage.getItem("accessToken")),
+        Authorization: "Bearer " + localStorage.getItem("accessToken"),
       },
       // credentials: "include", //to wystarczy by wyslac cookies
     };
     fetch("http://localhost:8000/dj-rest-auth/user", requestOptions)
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((data) => setUser(data))
       .catch((error) => console.error(error));
   }, []);
 
   return (
     <>
-      {/* {user?<div>
- 
-      <p>Welcome {user.fname} {user.lname}</p>
-
-      <img src={user.avatar} alt="avatar" width={100}></img>
+   { String(localStorage.getItem("user"))}
+    <h1>Profil</h1>
+    <br/>
+       <h2>Witaj <b>{user.email}</b></h2>
       
-</div>:''} */}
-      <h1>Profile</h1>
     </>
   );
 }

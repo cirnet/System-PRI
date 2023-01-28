8b851f02f867616b09eb5f1bab6570fb03aaa11a
cirnet <78275621+cirnet@users.noreply.github.com>
2023-01-28 21:53:52 +0100

    Praca nad tabelą komisji

--- frontend/src/Comission/Comission.js
+++ frontend/src/Comission/Comission.js
@@ -1,88 +1,123 @@
-import ComissionElement from "./ComissionElement"
 import { useEffect, useState } from "react";
-import axios from 'axios'
-import { DataGrid } from '@mui/x-data-grid'
-import './style.css'
-
-import DoNotDisturbOnIcon from '@mui/icons-material/DoNotDisturbOn';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-
-const divStyle = {
-  color: 'blue',
-};
+import axios from "axios";
+import { DataGrid, GridValueGetterParams } from "@mui/x-data-grid";
+import "./Comission.css";
+import moment from "moment";
+import DoNotDisturbOnIcon from "@mui/icons-material/DoNotDisturbOn";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const columns = [
-  { field: 'id', headerName: 'ID' },
-  { field: 'time_start', headerName: 'Od', width: 150,renderCell:(params)=>{return(<span>{new Date(params.row.time_start).toLocaleString()}</span>)} },
-  { field: 'time_end', headerName: 'Do', width: 150,renderCell:(params)=>{return(<span>{new Date(params.row.time_end).toLocaleString()}</span>)} },
-  { field: 'is_valid', headerName: 'is_valid', width: 150, renderCell:(params)=>{return(
-<>
-{params?"sdsd":"nie"}
-</>
-  
-  
-  
-  
-  )} },
-  { field: 'is_complete', headerName: 'is_complete', width: 150 },
-  { field: 'is_accepted', headerName: 'is_accepted', width: 150 },
-  { field: 'is_selected', headerName: 'is_selected', width: 150 }
-]
+  { field: "id", headerName: "ID" },
+  {
+    field: "time_end",
+    headerName: "Data",
+    width: 150,
+    renderCell: (params) => {
+      return <span>{moment(params.row.time_end).format("L")}</span>;
+    },
+  },
+  {
+    field: "time_start",
+    headerName: "Godzina",
+    width: 150,
+    renderCell: (params) => {
+      return <span>{moment(params.row.time_start).format("LT")}</span>;
+    },
+  },
+  {
+    field: "is_valid",
+    headerName: "Valid",
+    width: 150,
+    renderCell: (params) => {
+      return (
+        <>
+          {params.row.is_valid ? (
+            <CheckCircleIcon style={{ color: "green" }} />
+          ) : (
+            <DoNotDisturbOnIcon style={{ color: "red" }} />
+          )}
+        </>
+      );
+    },
+  },
+  {
+    field: "is_complete",
+    headerName: "Complete",
+    width: 150,
+    renderCell: (params) => {
+      return (
+        <>
+          {params.row.is_complete ? (
+            <CheckCircleIcon style={{ color: "green" }} />
+          ) : (
+            <DoNotDisturbOnIcon style={{ color: "red" }} />
+          )}
+        </>
+      );
+    },
+  },
+  {
+    field: "is_accepted",
+    headerName: "Accepted",
+    width: 150,
+    renderCell: (params) => {
+      return (
+        <>
+          {params.row.is_accepted ? (
+            <CheckCircleIcon style={{ color: "green" }} />
+          ) : (
+            <DoNotDisturbOnIcon style={{ color: "red" }} />
+          )}
+        </>
+      );
+    },
+  },
+  {
+    field: "is_selected",
+    headerName: "Selected",
+    width: 150,
+    renderCell: (params) => {
+      return (
+        <>
+          {params.row.is_selected ? (
+            <CheckCircleIcon style={{ color: "green" }} />
+          ) : (
+            <DoNotDisturbOnIcon style={{ color: "red" }} />
+          )}
+        </>
+      );
+    },
+  },
+];
 
-export default function Comission(){
-     const [content, setContent] = useState([])
+export default function Comission() {
+  const [content, setContent] = useState([]);
 
-    useEffect(()=>{
-      const fetch = async()=>{
-        const {data} = await axios.get('http://localhost:8000/api/commission/')
-            setContent(data)
-            console.log(data)
-      }
-        fetch()
-    },[])
-console.log(content)
-    console.log(content.is_valid)
+  useEffect(() => {
+    const fetch = async () => {
+      const { data } = await axios.get("http://localhost:8000/api/commission/");
+      setContent(data);
+      console.log(data);
+    };
+    fetch();
+  }, []);
 
-
-    if(content.length>0){
-    <DataGrid rows={content} columns={columns} pageSize={100} align="center"/>
-  }
-
-
-    return(
-<div className="content">
-
-
-<h1>Comission</h1>
-
-{content.length>0?<><div style={{ height: 500, width: '95%' }}>
-      <DataGrid
-        rows={content}
-        columns={columns}
-        pageSize={100}
-        align="center"/>
+  return (
+    <div className="content">
+      {content.length > 0 ? (
+        <>
+          <div
+            style={{
+              height: "80vh",
+              width: "90vw",
+            }}
+          >
+            <DataGrid rows={content} columns={columns} pageSize={100} />
+          </div>
+        </>
+      ) : (
+        "Brak wolnych slotow"
+      )}
     </div>
-</>
-:"Brak wolnych slotow"}
-
-
-
-
-{/* {content ? content.map(e=>(
-        <ComissionElement 
-        key={e.id}
-        id={e.id}
-        time_start={e.time_start}
-        time_end={e.time_end}
-        is_complete={e.is_complete}
-        is_accepted={e.is_accepted}
-        is_selected={e.is_selected}
-        is_valid={e.is_valid}
-        members={e.members}
-        />)
-    ):"Brak comission"} */}
-
-</div>
-    
-    )
+  );
 }
